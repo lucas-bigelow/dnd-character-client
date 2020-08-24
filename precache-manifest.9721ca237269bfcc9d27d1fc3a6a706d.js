22aa677466083b4deda6fbb41788ac9a8e71d698
Lucas Bigelow <lukejbigelow@gmail.com>
2020-08-24 11:39:47 -0500

    Updates

---
+++ precache-manifest.9721ca237269bfcc9d27d1fc3a6a706d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76c75a085b9eb797a7ba2d67d20da845",
+    "url": "/dnd-character-client/index.html"
+  },
+  {
+    "revision": "5abfd6bb0635f0d5e230",
+    "url": "/dnd-character-client/static/css/main.98e300ae.chunk.css"
+  },
+  {
+    "revision": "24cb2a68f71e1b7d136c",
+    "url": "/dnd-character-client/static/js/2.4613ce7d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/dnd-character-client/static/js/2.4613ce7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5abfd6bb0635f0d5e230",
+    "url": "/dnd-character-client/static/js/main.b17c8c8c.chunk.js"
+  },
+  {
+    "revision": "0d7bbd0995820943aefe",
+    "url": "/dnd-character-client/static/js/runtime-main.06d9f42a.js"
+  }
+]);
